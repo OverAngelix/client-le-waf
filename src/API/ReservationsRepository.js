b4a3976811b2ab8724752f927a46a9262ae26adc
Steven Copin <steven.copin@univ-lille.fr>
2021-09-08 23:11:55 +0200

    Change url

--- src/API/ReservationsRepository.js
+++ src/API/ReservationsRepository.js
@@ -6,14 +6,14 @@ export default {
 
   async getReservations(criteres) {
     const reservations = await axios.get(
-      "http://localhost:3001/api/reservations", { params: criteres }
+      "https://server-le-waf.herokuapp.com//api/reservations", { params: criteres }
     );
     return reservations.data;
   },
 
   async addReservation(reservation) {
     const response = await axios.post(
-      "http://localhost:3001/api/reservations",
+      "https://server-le-waf.herokuapp.com//api/reservations",
       reservation
     );
     return response.data;
