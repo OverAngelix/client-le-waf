b9a635519f8db838db8421999c8eb44eda2b21a2
Steven Copin <steven.copin@univ-lille.fr>
2021-11-07 15:29:00 +0100

    FIX remove switch theme'

--- src/vues/Carte.vue
+++ src/vues/Carte.vue
@@ -1,16 +1,17 @@
 <template>
   <v-container>
-    <v-row align="center" justify="center">
-      <v-img
-        alt="menu"
-        contain
-        class="mb-4"
-        max-height="1000"
-        max-width="2000"
-        :src="require(`@/assets/carte.jpg`)"
-      >
-      </v-img>
-    </v-row>
+    <v-row justify="center">
+      <v-col justify="center">
+        <v-img
+          alt="menu"
+          contain
+          class="mb-4"
+          max-height="1000"
+          max-width="2000"
+          :src="require(`@/assets/carte.jpg`)"
+        >
+        </v-img> </v-col
+    ></v-row>
   </v-container>
 </template>
 
