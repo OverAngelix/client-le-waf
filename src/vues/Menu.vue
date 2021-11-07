4d4b9e2b3d626fd75edc719469e35953cdbfb3ba
Steven Copin <steven.copin@univ-lille.fr>
2021-11-07 14:38:14 +0100

    FIX : footer

--- src/vues/Menu.vue
+++ src/vues/Menu.vue
@@ -31,35 +31,40 @@
     </v-card>
 
     <v-footer dark padless>
-      <v-card flat tile class="blue lighten-3 text-center">
+      <v-card flat tile class="blue lighten-3 flex white--text text-center">
         <v-card-text>
           <v-btn
-            v-for="icon in icons"
-            :key="icon"
-            class="mx-4 white--text"
+            href="https://www.facebook.com/lewaf/"
+            target="_blank"
+            class="mx-4"
+            color="black"
             icon
           >
-            <v-icon size="24px">
-              {{ icon }}
-            </v-icon>
+            <v-icon size="24px"> mdi-facebook </v-icon>
           </v-btn>
-        </v-card-text>
-
-        <v-card-text class="black--text pt-0">
-          Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet.
-          Mauris cursus commodo interdum. Praesent ut risus eget metus luctus
-          accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim
-          a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula
-          lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus
-          iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum
-          tempor vel ut orci. Orci varius natoque penatibus et magnis dis
-          parturient montes, nascetur ridiculus mus.
+          <v-btn
+            href="https://www.instagram.com/le.waf/"
+            target="_blank"
+            class="mx-4"
+            color="black"
+            icon
+          >
+            <v-icon size="24px"> mdi-instagram </v-icon>
+          </v-btn>
+          <v-btn
+            href="https://vm.tiktok.com/ZM8QfLLjG/"
+            target="_blank"
+            class="mx-4"
+          >
+            Tiktok
+          </v-btn>
         </v-card-text>
 
         <v-divider></v-divider>
 
         <v-card-text class="black--text">
-          Copyright 2021, Steven Copin
+          Copyright {{ new Date().getFullYear() }} —
+          <strong>Steven Copin</strong>
         </v-card-text>
       </v-card>
     </v-footer>
@@ -96,12 +101,7 @@ export default {
       "LES COPAINS DU WAF",
       "WAF IN ENGLISH",
     ],
-     icons: [
-        'mdi-facebook',
-        'mdi-twitter',
-        'mdi-linkedin',
-        'mdi-instagram',
-      ],
+    icons: ["mdi-facebook", "mdi-instagram"],
   }),
 
   methods: {},
