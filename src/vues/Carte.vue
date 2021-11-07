6f90cf733c2eb44ffcc4507ddb14d102276668d9
Steven Copin <steven.copin@univ-lille.fr>
2021-11-07 15:20:33 +0100

    FIX : image

--- src/vues/Carte.vue
+++ src/vues/Carte.vue
@@ -1,17 +1,16 @@
 <template>
   <v-container>
     <v-row align="center" justify="center">
-      <v-col align="center" justify="center">
-        <v-img
-          alt="menu"
-          contain
-          class="mb-4"
-          max-height="1000"
-          max-width="2000"
-          :src="require(`@/assets/carte.jpg`)"
-        >
-        </v-img> </v-col
-    ></v-row>
+      <v-img
+        alt="menu"
+        contain
+        class="mb-4"
+        max-height="1000"
+        max-width="2000"
+        :src="require(`@/assets/carte.jpg`)"
+      >
+      </v-img>
+    </v-row>
   </v-container>
 </template>
 
